34aeb94fca4ef63a894b39decbfd72cdec43747b
farah albassam <1402@rawdalsaleheen.edu.kw>
2022-08-15 01:52:21 +0300

    cw 6 done

---
+++ script.js
@@ -0,0 +1,19 @@
+alert("hola this site is going to calculate your grade");
+
+let grade = prompt("type your grade");
+
+console.log("grade");
+
+if (grade => 90) {
+    console.log("exclent");
+} else if (grade >= 80) {
+    console.log("very good");
+} else if (grade >= 70) {
+    console.log("good");
+} else if (grade >= 60) {
+    console.log("aceppted");
+} else if (grade >= 50) {
+    console.log("need to improve");
+} else {
+    console.log(failed);
+}
